2b5521f4d64a3bbe9eb5c4c275cf62ad6e225fba
Dimme <d.serakis@gmail.com>
2013-03-20 15:21:07 +0100

    printing out some text to se if jekins will log it!

--- GitJenkins/src/GitIsFun/TestCalenderClass.java
+++ GitJenkins/src/GitIsFun/TestCalenderClass.java
@@ -10,11 +10,13 @@ public class TestCalenderClass {
 	public void test_getValue() {
 		int nr = 4;
 		CalenderClass ex1 = new CalenderClass(nr, "Meet");
+		System.out.println("Testing function getValue()");
 		assertTrue(4 == ex1.getValue());
 	}
 	public void test_getName(){
 		String name = "Meeting NOW";
 		CalenderClass ex2 = new CalenderClass(4, "me");
+		System.out.println("Testing function getName()");
 		assertTrue(name == ex2.getName());
 	}
 }
